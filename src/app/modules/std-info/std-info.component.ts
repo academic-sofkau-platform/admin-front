9b5c678b78706921249f889eafd85eca7743d75b
giannibaccino <giannibaccino@gmail.com>
2022-09-07 18:01:10 -0300

    Adds student hardcoded info and chart

--- src/app/modules/std-info/std-info.component.ts
+++ src/app/modules/std-info/std-info.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Chart from 'chart.js/auto';
+import { Student } from 'src/app/shared/models/student';
 
 @Component({
   selector: 'app-std-info',
@@ -8,41 +9,34 @@ import Chart from 'chart.js/auto';
 })
 export class StdInfoComponent implements OnInit {
 
-  constructor() { }
+    std: Student;
+
+  constructor() { 
+    this.std = {
+        id: "007",
+        name: "Gianni",
+        lastname: "Baccino",
+        city: "Montevideo",
+        gender: "M",
+        email: "giannib@gmail.com",
+        phoneNumber: 1234,
+        photo:"urldefoto",
+        bilingual: true
+    };
+  }
 
   ngOnInit() {
     const stdChart = new Chart("stdChart", {
-      type: 'bar',
+      type: 'line',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          labels: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'],
           datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
+              label: 'Notas',
+              data: [0, 65, 59, 80, 81, 56, 55, 40, 100],
+              fill: false,
+              borderColor: 'rgb(255, 140, 0)',
+              tension: 0.1
           }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
       }
   });
   }
