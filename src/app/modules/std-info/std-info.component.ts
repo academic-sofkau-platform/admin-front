6f18e09c87fb21e6a3e68f5a082f86435f88b976
giannibaccino <giannibaccino@gmail.com>
2022-09-07 15:37:41 -0300

    Second commit

---
+++ src/app/modules/std-info/std-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-std-info',
+  templateUrl: './std-info.component.html',
+  styleUrls: ['./std-info.component.css']
+})
+export class StdInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
