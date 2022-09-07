48b4d1ccbc55bf946fc8fbf210449090c635b9f4
nicovelar <nicovelar@gmail.com>
2022-09-07 19:33:46 -0300

    Vista de los Cursos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { StdInfoComponent } from './modules/std-info/std-info.component';
+import { CursosComponent } from './pages/cursos/cursos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/std-info', pathMatch: 'full' },
   { path: 'std-info', component: StdInfoComponent },
+  { path: 'cursos', component:CursosComponent }
 ];
 
 @NgModule({
