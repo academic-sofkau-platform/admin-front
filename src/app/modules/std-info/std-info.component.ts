e14e3bb8693859ca54629bb3bd4ced5dac68c662
giannibaccino <giannibaccino@gmail.com>
2022-09-09 19:43:17 -0300

    Adds heatmap reactive functions

--- src/app/modules/std-info/std-info.component.ts
+++ src/app/modules/std-info/std-info.component.ts
@@ -5,86 +5,178 @@ import { Student } from 'src/app/shared/models/student';
 @Component({
   selector: 'app-std-info',
   templateUrl: './std-info.component.html',
-  styleUrls: ['./std-info.component.css']
+  styleUrls: ['./std-info.component.css'],
 })
 export class StdInfoComponent implements OnInit {
-  margin = { left: 0, right: 0, top: 0, bottom: 0 };
-
   cellSettings = {
     border: {
-        width: 0.5,
-        color: 'white'
-    }
-};
+      width: 0.5,
+      color: 'white',
+    },
+  };
 
-  palleteSettings ={
-    palette: [{  color: '#c1f0f0 ', label:'Poor' },
-            {  color: '#5cd3fb ', label:'Average' },
-            { color: '#111979 ', label:'Excellent' }              
-        ],
-        type: "Gradient"
+  palleteSettings = {
+    palette: [
+      { color: '#c1f0f0 ', label: 'Poor' },
+      { color: '#5cd3fb ', label: 'Average' },
+      { color: '#111979 ', label: 'Excellent' },
+    ],
+    type: 'Gradient',
   };
 
   titleSettings = {
-    text: "Progreso",
+    text: 'Progreso',
     textStyle: {
-      size: '18px',
+      size: '24px',
+      color: 'black',
       fontWeight: '500',
-      fontStyle: 'Bold'
-}};
-
-  dataSource = [
- 
-    [73, 39, 26, 39, 94, 0, 23],
- 
-    [93, 58, 53, 38, 26, 68, 72],
- 
-    [99, 28, 22, 4, 66, 90, 91],
- 
-    [14, 26, 97, 69, 69, 3, 53],
- 
-    [7, 46, 47, 47, 88, 6, 12],
- 
-    [41, 55, 73, 23, 3, 79, 7],
- 
-    [56, 69, 21, 86, 3, 33, 28],
- 
-    [45, 7, 53, 81, 95, 79, 49],
- 
-    [60, 77, 74, 68, 88, 51, 9]
- 
-  ];
-  xAxis = {
- 
-    labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4', 'Semana 5', 'Semana 6', 'Semana 7',
- 
-      'Semana 8', 'Semana 9'],
- 
+      fontStyle: 'Bold',
+    },
   };
- 
+
+  dataSource: any[] = [];
+  actividad: any[] = [
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-01'),
+      puntaje: 12
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-02'),
+      puntaje: 30
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-03'),
+      puntaje: 15
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-04'),
+      puntaje: 18
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-05'),
+      puntaje: 21
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-06'),
+      puntaje: 3
+    },
+    {
+      cursoId: "12312",
+      aprendizId: "rauuuuuul",
+      fecha: new Date('2022-09-07'),
+      puntaje: 0
+    },
+    // {
+    //   cursoId: "12312",
+    //   aprendizId: "rauuuuuul",
+    //   fecha: new Date('2022-09-23'),
+    //   puntaje: 33
+    // },
+    // {
+    //   cursoId: "12312",
+    //   aprendizId: "rauuuuuul",
+    //   fecha: new Date('2022-09-30'),
+    //   puntaje: 33
+    // },
+    // {
+    //   cursoId: "12312",
+    //   aprendizId: "rauuuuuul",
+    //   fecha: new Date('2022-10-15'),
+    //   puntaje: 24
+    // },
+  ];
+
+  dataSourceSettings = {
+    isJsonData: true,
+    adaptorType: 'Cell',
+    xDataMapping: 'Labels.Xlabel',
+    yDataMapping: 'Labels.Ylabel',
+    valueMapping: 'data.value',
+  };
+
+  xAxis = {
+    labels: ['Semana 1', 'Semana 2', 'Semana 3','Semana 4','Semana 5', 'Semana 6', 'Semana 7'], //cantidad de semanas de duracion dado al crear el training
+
+    textStyle: {
+      size: '14px',
+      color: 'black',
+      fontWeight: '500',
+      fontStyle: 'Bold',
+    },
+  };
+
   yAxis = {
- 
-    labels: ['Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab', 'Dom'],
- 
-  }
+    labels: ['Dom', 'Sab', 'Vie', 'Jue', 'Mie', 'Mar', 'Lun'],
 
-    std: Student;
+    textStyle: {
+      size: '14px',
+      color: 'black',
+      fontWeight: '500',
+      fontStyle: 'Bold',
+    },
+  };
 
-  constructor() { 
+  std: Student;
+
+  constructor() {
     this.std = {
-        id: "007",
-        name: "Nombre",
-        lastname: "Apellido",
-        city: "Ciudad",
-        gender: "M",
-        email: "example@mail.com",
-        phoneNumber: 59899123456,
-        photo:"urldefoto",
-        bilingual: true
+      id: '007',
+      name: 'Nombre',
+      lastname: 'Apellido',
+      city: 'Ciudad',
+      gender: 'M',
+      email: 'example@mail.com',
+      phoneNumber: 59899123456,
+      photo: 'urldefoto',
+      bilingual: true,
     };
   }
 
   ngOnInit() {
+    this.actividad.forEach(cosahecha => {
+      this.dataSource.push({'Labels': {'Xlabel': 'Semana ' + this.getWeeksDiff(new Date('2022-09-01'), cosahecha.fecha) , 'Ylabel': this.getDayOfWeek(cosahecha.fecha.getDay())}, 'data': {'value': cosahecha.puntaje}});
+    });
   }
 
+  getWeeksDiff(startDate: Date, endDate: Date) {
+    const msInWeek = 1000 * 60 * 60 * 24 * 7;
+
+    return (
+      Math.round(Math.abs(endDate.getTime() - startDate.getTime()) / msInWeek) +
+      1
+    );
+  }
+
+  getDayOfWeek(day: number) : string {
+    switch (day) {
+      case 0:
+        return 'Lun';
+      case 1:
+        return 'Mar';
+      case 2:
+        return 'Mie';
+      case 3:
+        return 'Jue';
+      case 4:
+        return 'Vie';
+      case 5:
+        return 'Sab';
+      case 6:
+        return 'Dom';
+      default:
+        return 'Default';
+    }
+  }
 }
