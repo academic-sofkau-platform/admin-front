ef4bd0eff26aa07daab3e6d5ebd770cc4a08eec5
eXtreme Training <61291067+Sofka-XT@users.noreply.github.com>
2022-09-14 09:43:06 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
+  apiBase: "https://academic-sofkau.herokuapp.com",
   firebase: {
-    projectId: 'proyecto-final-92a95',
-    appId: '1:506066550812:web:4114aa5daadb93af47dfd9',
-    storageBucket: 'proyecto-final-92a95.appspot.com',
-    apiKey: 'AIzaSyCTcv4x0AXRcoBZp02GSG_smfjLbLwYDF8',
-    authDomain: 'proyecto-final-92a95.firebaseapp.com',
-    messagingSenderId: '506066550812',
+   apiKey: "AIzaSyD-RlhMjFAzFQx7NQ4qJxS-Wcnq4ojqK10",
+   authDomain: "academic-sofkau.firebaseapp.com",
+   projectId: "academic-sofkau",
+  storageBucket: "academic-sofkau.appspot.com",
+  messagingSenderId: "166954431477",
+  appId: "1:166954431477:web:35d0d54e698fec3e5666bc"
   },
   production: true
 };
