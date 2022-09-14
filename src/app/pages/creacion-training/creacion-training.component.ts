769cce12016ff5fd03099682beeae4add26965ec
Fabricio <fabribcom@gmail.com>
2022-09-13 21:26:26 -0300

    base64 funcionando, nueva vista conectada, modelo training agregado

--- src/app/pages/creacion-training/creacion-training.component.ts
+++ src/app/pages/creacion-training/creacion-training.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { TrainingModel } from 'src/app/shared/models/training';
 import { ApiService } from 'src/app/shared/services/api.service';
+import { Observable, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-creacion-training',
@@ -10,23 +11,14 @@ import { ApiService } from 'src/app/shared/services/api.service';
 })
 export class CreacionTrainingComponent {
 
-  dataSource: TrainingModel[] = [];
-  trainingForm: FormGroup;
+  csvFile: any;//?
+  csvBase64 : string = "";
+  csvFileName: string = "";
+
   constructor(
     public api: ApiService,
     private formBuilder: FormBuilder
-  ) {
-    this.trainingForm = new FormGroup({
-      nombre: new FormControl(),
-      descripcion: new FormControl(),
-      fechaInicio: new FormControl(),
-      fechaFinal: new FormControl(),
-    })
-  }
-
-  csvFile: any;//?
-  csvBase64: string | any = "";
-  csvFileName: string = "";
+  ) {}
 
   public miFormulario: FormGroup = this.formBuilder.group({
     nombre: [, [Validators.required, Validators.minLength(3)], []],
@@ -35,54 +27,41 @@ export class CreacionTrainingComponent {
     descripcion: [, [Validators.required, ], []],
     coach: [, [Validators.required ]],//select
     ruta: [, [Validators.required ]],//select leer validators
-    csvFile: [, [Validators.required]]//file csv base64
+    csvBase64input: [, [Validators.required, Validators.minLength(300)], []]
   })
 
-  convertToBase64(event: any) {
-    const file:File = event.target.files[0];
-    this.csvFile = file;//?
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.csvFileName = file.name;
+    this.convertToBase64(file).subscribe(base64 => {
+      this.csvBase64 = base64;
+    });
+  }
 
-    if (file) {
-
-        this.csvFileName = file.name;
-
-        const formData = new FormData();
-
-        formData.append("thumbnail", file);
-
-        //const upload$ = this.http.post("/api/thumbnail-upload", formData);
-
-        // upload$.subscribe();
-        console.log(this.csvFileName);
-    }
+  convertToBase64(file : File) : Observable<string> {
+    const result = new ReplaySubject<string>(1);
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (event: any) => result.next(btoa(event.target.result.toString()));
+    return result;
   }
 
   enviarFormulario(): void {
     console.log("enviado");
-    console.log(this.miFormulario);
-
+    //console.log(this.csvBase64);
+      // console.log(this.miFormulario.get("nombre")?.value);
+      // console.log(this.miFormulario.get("fechainicio")?.value);
+      // console.log(this.miFormulario.get("fechafinal")?.value);
+      // console.log(this.miFormulario.get("descripcion")?.value);
+      // console.log(this.miFormulario.get("coach")?.value);
+      // console.log(this.miFormulario.get("ruta")?.value);
+      // console.log(this.miFormulario.get("csvBase64input")?.value);
+    console.log(this.miFormulario.value);
+    //console.log(this.csvBase64,"this.csvBase64");
     //this.api.crearTraining();
   }
   //--------------------------------------------------------------//testing
-  // fileName:string = '';
 
-  // onFileSelected(event: any) {
-
-  //     const file:File = event.target.files[0];
-
-  //     if (file) {
-
-  //         this.fileName = file.name;
-
-  //         const formData = new FormData();
-
-  //         formData.append("thumbnail", file);
-
-  //         const upload$ = this.http.post("/api/thumbnail-upload", formData);
-
-  //         upload$.subscribe();
-  //     }
-  // }
 }
 
 
