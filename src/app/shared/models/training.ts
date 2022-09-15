70ebc63deef22c21696dc26f0e86941f29f045ba
Joaquin <joacofum@gmail.com>
2022-09-14 21:25:48 -0300

    error en training.ts solucionado

--- src/app/shared/models/training.ts
+++ src/app/shared/models/training.ts
@@ -1,16 +1,8 @@
 
 export interface TrainingModel {
-<<<<<<< HEAD
-    trainingId: string;
-    name: string;
-    description: string;
-    startDate: number;
-    endDate: number;
-=======
     id: string;
     nombre: string;
     descripcion: string;
     fechaInicio: Date;
     fechaFinal: Date;
->>>>>>> dcc9d23ecd017353f0690d73a80b210663a1bf0d
     }
