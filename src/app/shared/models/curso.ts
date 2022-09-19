12c8715cd86b5a3699db0b19668220f49f773e44
nicovelar <nicovelar@gmail.com>
2022-09-19 17:09:47 -0300

    Acción de mejora

--- src/app/shared/models/curso.ts
+++ src/app/shared/models/curso.ts
@@ -2,6 +2,7 @@ export interface CursoModel {
     id: string;
     nombre: string;
     descripcion: string;
+    accionMejora: string;
     consigna: string;
     enlace: string;
     aprobacion: number;
